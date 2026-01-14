27525ad0dd5fe87e97124dc87b06de8089f4e7df
drew <github@andbrant.com>
2026-01-14 10:26:19 -0600

    get ip from reporting service

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 source <(sops -d ./.enc.env)
+
+ip_log_file="/tmp/.ipaddress"
+
+trap 'rm "$ip_log_file"' EXIT
+
+curl -H "Authorization:Bearer $ntfy_token" "$ntfy_endpoint/$ntfy_topic/json?since=5m&poll=1" | 
+  jq -r .message |
+  jq -r 'select(.device_name | test("vm-nix-vm")) .pub_ip' |
+  head -1 > $ip_log_file 
+
+export TF_VAR_home_ip="`cat $ip_log_file`"
+
+echo "TF_VAR_home_ip is $TF_VAR_home_ip"
+
 # tofu init
 tofu plan -out ./plan.hcl
 && tofu apply ./plan.hcl
